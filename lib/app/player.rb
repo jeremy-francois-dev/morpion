27ee798549ecba8cf2113101688ddc914175eddc
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-10-17 11:44:39 +0200

    update

---
+++ lib/app/player.rb
@@ -0,0 +1,13 @@
+class Player
+  attr_accessor :name, :symbol
+  
+  def initialize(symbol)
+    puts " Bienvenue dans notre jeu  de Morpion !"
+    puts " Quel est ton prénom"
+    print ">"
+    @name = gets.chomp.to_s
+    @symbol = symbol
+    puts " Super #{@name}, le symbole '#{@symbol}' t'a été attribué !"
+  end
+
+end
