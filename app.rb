2c267dabe5849b662ec6f99bb5f1ab5c484ab5c7
guillaume <gueyome@hotmail.fr>
2019-10-17 16:31:33 +0200

    bingo

--- app.rb
+++ app.rb
@@ -7,16 +7,26 @@ require 'app/board'
 require 'app/game'
 
 def perform
-  game = Game.new
-  while game.victory? == false && game.draw? == false
-    game.board_game.show
-    game.placement(game.player1)
-    game.board_game.show
-    if game.victory? == false && game.draw? == false
-      game.placement(game.player2)
+  decision = "YES"
+  while decision == "YES"
+    game = Game.new
+    while game.victory? == false && game.draw? == false
+      game.board_game.show
+      game.placement(game.player1)
+      game.board_game.show
+      if game.victory? == false && game.draw? == false
+        game.placement(game.player2)
+      end
+    end
+    game.show_result
+    
+    puts "Souhaitez-vous maintenant refaire une partie ? Entrez 'YES' or 'NO' please."
+    decision = gets.chomp
+    while decision != "YES" && decision != "NO"
+      puts "Merci de rentrer 'YES' or 'NO' avec format"
+      decision = gets.chomp
     end
   end
-  game.show_result
 end
 
 perform
